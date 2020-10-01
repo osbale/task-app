594c2be13209a3eaaac9b12a1e266097bfb9b300
unknown <oskaras900@gmail.com>
2020-10-01 14:59:26 +0300

    Updates

---
+++ precache-manifest.a7e10716227b7e69d78dc05dd1014912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a8e0fc33b3f218b8a91e204a4ca4a6",
+    "url": "/task-app/index.html"
+  },
+  {
+    "revision": "eebe01b2ea1f9a0d1226",
+    "url": "/task-app/static/css/main.aaf126e7.chunk.css"
+  },
+  {
+    "revision": "13c152f2a3163be1f7ac",
+    "url": "/task-app/static/js/2.f362d7e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-app/static/js/2.f362d7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebe01b2ea1f9a0d1226",
+    "url": "/task-app/static/js/main.b601e958.chunk.js"
+  },
+  {
+    "revision": "f3a2472f8d47fdcd861d",
+    "url": "/task-app/static/js/runtime-main.eda20655.js"
+  }
+]);
